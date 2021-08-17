a10a05c8cdb489e9d1597ca6c75a83a082cc2768
Mauro Ojeda <88348571+maojeda21@users.noreply.github.com>
2021-08-17 20:31:33 -0300

    Update 2.10 Ejercicios.py

--- 2.10 Ejercicios.py
+++ 2.10 Ejercicios.py
@@ -14,3 +14,13 @@ print(type(edad_cadena))
 edad_2035=(2035-2021)+edad
 print(edad_2035)
 
+###
+# 2) Al realizar una consulta en un registo hemos recibido unos valores corruptos. Al parecer entrega el nombre y apellido del alumno al reves.
+#  ¿que puede hacer para obtener el siguiente mensaje por pantalla?
+###
+
+cadena="azebaC ellehciM , 01"
+cadena_volteada=cadena[::-1] # los corchetes indican indexacion, los dos puntos seguidos significa que va a tomar todos los valores dentro, el -1 arranca de atras para adealnte
+print(cadena_volteada)
+
+print(cadena_volteada[4::], "ha sacado un ",cadena_volteada[:2])
